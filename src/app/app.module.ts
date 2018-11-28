ed82ed31d12f4befa1aa70987ebf301a8b565980
Aravind Govardhana <govardhana.aravind@live.com>
2018-11-28 12:37:43 +0530

    Include services in Providers.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ErrorHandler, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './components/app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AlertService } from './services/alert.service';
+import { BackendService } from './services/backend.service';
+import { ErrorsHandler } from './services/error-handler.service';
+import { LoggerService } from './services/logger.service';
+import { SecuredStorageProviderService } from './services/secured-storage-provider.service';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpInterceptors } from './services/http-interceptors';
 
 @NgModule({
   declarations: [
@@ -14,7 +21,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     AppRoutingModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    AlertService,
+    BackendService,
+    ErrorsHandler,
+    LoggerService,
+    SecuredStorageProviderService,
+    {provide: ErrorHandler, useClass: ErrorsHandler},
+    {provide: HTTP_INTERCEPTORS, useClass: HttpInterceptors, multi: true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
