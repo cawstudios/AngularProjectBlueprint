db251e19b464a98c5dba36724aaa50269a1a7550
Aravind Govardhana <govardhana.aravind@live.com>
2018-12-21 16:43:17 +0530

    Add build script.

--- build.sh
+++ build.sh
@@ -1,12 +1,5 @@
-#!/usr/bin/env bash
 ENVIRONMENT=$1
-VERSION=`git describe --tags --long`
-sed -i "s/%VERSION%/$VERSION/g" ./src/environments/environment.${ENVIRONMENT}.ts
 npm install
-npm run build:${ENVIRONMENT}
+npm run build:$ENVIRONMENT
 mkdir -p dist/source-map-files
 mv dist/RGIHomeInsurance/**.map dist/source-map-files
-npm install -g @sentry/cli --unsafe-perm
-sentry-cli releases new "$VERSION" --finalize
-sentry-cli releases files ${VERSION} upload-sourcemaps ./dist/source-map-files
-sentry-cli releases deploys ${VERSION} new -e ${ENVIRONMENT} -n ${VERSION}
