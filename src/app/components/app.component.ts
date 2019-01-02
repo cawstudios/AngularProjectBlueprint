1b1120cb50b0d11815bff5ae1435c8750d801766
Aravind Govardhana <govardhana.aravind@live.com>
2019-01-02 16:25:31 +0530

    Implement Base Component.

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -1,15 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Injector, OnDestroy, OnInit } from '@angular/core';
 import { AlertService } from '../services/alert.service';
+import { BaseComponent } from '../base.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent extends BaseComponent implements OnDestroy, OnInit {
   title = 'AngularProjectBlueprint';
 
-  constructor(private readonly alertService: AlertService) {
+  constructor(
+    private injector: Injector,
+    private readonly alertService: AlertService) {
+    super('AngularProjectBlueprint', injector);
+  }
+
+  ngOnDestroy() {
+    super.ngOnDestroy();
   }
 
   sendAlert(): void {
